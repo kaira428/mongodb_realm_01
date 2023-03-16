aafe04d3c1a565f92a09800557133a6adec49d62
Wong Keng <kaira428@gmail.com>
2023-03-16 17:57:35 +0800

    Use tenary operator

--- components/Movie.js
+++ components/Movie.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Movie = (props) => {
   return (
-    <div style={{textAlign: 'center'}}>
-        <h3>
-            {props.title}
-        </h3>
-        <br />
-        {props.plot && <p style={{color: 'cyan'}}>{props.plot}</p>}
-        {!props.plot && <p style={{color: 'red'}}>No Plot Found</p>}
-       <br />
-       <br />
+    <div style={{ textAlign: "center" }}>
+      <h3>{props.title}</h3>
+      <br />
+      {/* {props.plot && <p style={{color: 'cyan'}}>{props.plot}</p>}
+        {!props.plot && <p style={{color: 'red'}}>No Plot Found</p>} */}
+      {props.plot ? (
+        <p style={{ color: "cyan" }}>{props.plot}</p>
+      ) : (
+        <p style={{ color: "red" }}>No Plot Found</p>
+      )}
+      <br />
+      <br />
     </div>
-  )
-}
+  );
+};
 
-export default Movie
+export default Movie;
